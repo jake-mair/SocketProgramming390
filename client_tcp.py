d3c1753c79b5e7a495802b17e1df18881266a697
Jake Mair <jmair@hamilton.edu>
2024-10-31 20:05:12 -0400

    Working put and get

--- client_tcp.py
+++ client_tcp.py
@@ -1,41 +1,44 @@
 from socket import *
 import sys
+import os
 
 # Function to send a file from the client to the server
 def sendFile(filename, clientSocket):
+    file_size = os.path.getsize(filename)
+    clientSocket.sendall(f"{file_size}".encode())
+    clientSocket.recv(1024)
+
+
     with open(filename, 'rb') as file:
         while True:
             data = file.read(1024)  # Read the file in 1024-byte chunks
-            print([data])
-            clientSocket.sendall(data)  # Send the file data to the server
             if not data:  # If no data left, break the loop
-                print("entered")
                 break
-        file.close()
-
-'''I need to send something signifiying the end of the file rn it gets stuck and just waits to receive
-also I don't understand how to do that with the chunks of bytes because if I were to signify that it was
-the end of file then the entire chunk isn't equal to that'''
-
-'''It's just not finding the end signifier on the server side'''
-
+            clientSocket.sendall(data)  # Send the file data to the server
 
 
 # Function to receive a file from the server and save it
 def recFile(filename, clientSocket):
+
+    file_size = int(clientSocket.recv(1024).decode())
+    clientSocket.sendall(b"File size received.")
+
     # Rename the file if it already exists by appending '_1'
     parts = filename.rsplit('.', 1)  # Split the filename and extension
     name = parts[0]
     ext = parts[1]
     new_filename = f"{name}_1.{ext}"  # Create a new filename to avoid overwriting
 
+    bytes_received = 0 
     # Open the new file for writing binary data
     with open(new_filename, 'wb') as file:
-        while True:
+        while bytes_received < file_size:
             data = clientSocket.recv(1024)  # Receive data in 1024-byte chunks
             if not data:  # If no more data, break the loop
                 break
             file.write(data)  # Write the data to the new file
+            bytes_received += len(data)
+    clientSocket.sendall(b"Client response: File downloaded.")
 
 # Main client function to handle user commands and communicate with the server
 def startClient(name, port):
@@ -61,35 +64,35 @@ def startClient(name, port):
             print("Server response: File uploaded.")
 
         # If the command is 'get', handle file download
-        # elif command[0] == "get":
-        #     filename = command[1]  # Get the filename from the command
-        #     clientSocket.sendall(b"get")  # Send the 'get' command to the server
-        #     clientSocket.recv(1024)  # Wait for the server to acknowledge
+        elif command[0] == "get":
+            filename = command[1]  # Get the filename from the command
+            clientSocket.sendall(b"get")  # Send the 'get' command to the server
+            clientSocket.recv(1024)  # Wait for the server to acknowledge
 
-        #     clientSocket.sendall(filename.encode())  # Send the filename to the server
-        #     clientSocket.recv(1024)  # Wait for the server to acknowledge
+            clientSocket.sendall(filename.encode())  # Send the filename to the server
+            clientSocket.recv(1024)  # Wait for the server to acknowledge
 
-        #     recFile(filename, clientSocket)  # Call the recFile function to download the file
-        #     clientSocket.recv(1024)  # Wait for server's confirmation
-        #     print(f"File {filename} downloaded.")
+            recFile(filename, clientSocket)  # Call the recFile function to download the file
+            clientSocket.recv(1024)  # Wait for server's confirmation
+            print(f"File {filename} downloaded.")
 
         # # If the command is 'keyword', handle file anonymization
-        # elif command[0] == "keyword":
-        #     keyword = command[1]  # Get the keyword from the command
-        #     filename = command[2]  # Get the filename from the command
+        elif command[0] == "keyword":
+            keyword = command[1]  # Get the keyword from the command
+            filename = command[2]  # Get the filename from the command
 
-        #     clientSocket.sendall(b"keyword")  # Send the 'keyword' command to the server
-        #     clientSocket.recv(1024)  # Wait for the server to acknowledge
+            clientSocket.sendall(b"keyword")  # Send the 'keyword' command to the server
+            clientSocket.recv(1024)  # Wait for the server to acknowledge
 
-        #     clientSocket.sendall(f"{keyword} {filename}".encode())  # Send the keyword and filename
-        #     response = clientSocket.recv(1024)  # Receive the server's response
-        #     print(response.decode())  # Print the server's response
+            clientSocket.sendall(f"{keyword} {filename}".encode())  # Send the keyword and filename
+            response = clientSocket.recv(1024)  # Receive the server's response
+            print(response.decode())  # Print the server's response
 
-        # # If the command is 'quit', handle client exit
-        # elif command[0] == "quit":
-        #     clientSocket.sendall(b"quit")  # Send the 'quit' command to the server
-        #     response = clientSocket.recv(1024).decode()  # Receive and decode the server's response
-        #     break  # Exit the loop and close the client
+        # If the command is 'quit', handle client exit
+        elif command[0] == "quit":
+            clientSocket.sendall(b"quit")  # Send the 'quit' command to the server
+            # clientSocket.recv(1024)  # Receive and decode the server's response
+            break  # Exit the loop and close the client
 
     clientSocket.close()  # Close the socket after quitting
 
