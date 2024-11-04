dbfaaef1c305347be2140afa3d971e17df222a24
brian tran <btran@hamilton.edu>
2024-11-03 19:01:38 -0500

    print?

--- server_tcp.py
+++ server_tcp.py
@@ -90,6 +90,9 @@ def keywordTask(conn):
     conn.sendall(
         f"Server response: File {filename} anonymized. Output file is {anonymized_filename}".encode()
     )
+    print(
+        f"Server response: File {filename} anonymized. Output file is {anonymized_filename}"
+    )
 
 
 # Function to start the server and listen for incoming client connections and commands
